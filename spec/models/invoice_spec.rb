9d772e9d148da534096c3553c9db0a92b25a51fb
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-06-13 14:10:32 -0600

    corrects grand total percentage off to reflect percentage off value of total reveniue

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -9,15 +9,17 @@ RSpec.describe Invoice, type: :model do
 
     @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
 
-    @coupon_1 = create(:coupon, merchant: @merchant1, status: 1, discount: 50, discount_type: 1) #100.00 total revenue, #$50.00 grand total
+    @coupon_1 = create(:coupon, merchant: @merchant1, status: 1, discount: 50, discount_type: 1) #90.00 total revenue, #$40.00 grand total
     @coupon_2 = create(:coupon, merchant: @merchant1, status: 1, discount: 50, discount_type: 0) #10.00 total, 5.00 grand total
+
     @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, coupon_id: @coupon_1.id, created_at: "2012-03-27 14:54:09")
     @invoice_2 = Invoice.create!(customer_id: @customer_1.id, status: 2, coupon_id: @coupon_2.id, created_at: "2012-03-28 14:54:09")
+    @invoice_3 = Invoice.create!(customer_id: @customer_1.id, status: 2, coupon_id: @coupon_1.id, created_at: "2012-03-28 14:54:09")
 
     @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 9, unit_price: 10, status: 2)
     @ii_11 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 1, unit_price: 10, status: 1)
     @ii_2 = InvoiceItem.create!(invoice_id: @invoice_2.id, item_id: @item_1.id, quantity: 1, unit_price: 10, status: 2)
-
+    @ii_3 = InvoiceItem.create!(invoice_id: @invoice_3.id, item_id: @item_1.id, quantity: 9, unit_price: 10, status: 2)
   end
 
   describe "validations" do
@@ -39,12 +41,11 @@ RSpec.describe Invoice, type: :model do
     end
 
     it '#grand_total(dollars off)' do 
-      require 'pry'; binding.pry
-      expect(@invoice_1.grand_total).to eq(50.00)
+      expect(@invoice_3.grand_total).to eq(40.00)
     end
     
     it '#grand_total(percentage off)' do 
-      expect(@invoice_1.grand_total).to eq(5.00)
+      expect(@invoice_2.grand_total).to eq(9.5)
     end
   end
 end
